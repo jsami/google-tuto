63dafa9f0a2f5abe5f778af3ebbb4c6d5c60c0b0
jsami <rj.sami21@outlook.com>
2017-07-08 19:07:07 +0400

    string manipulation

---
+++ string_manip.cpp
@@ -0,0 +1,19 @@
+#include <string>
+#include <iostream>
+using namespace std;
+
+int main() {
+  string str1 = "To be or not to be, that is the question";
+  string str2 = "only ";
+  string str3 = str1.substr(6, 12); // str3 = "or not to be"
+  str1.insert(32, str2); // str1 = "To be or not to be, that is the only question"
+  str1.replace(str1.find("to be", 0), 5, "to jump"); // str1 =  "To be or not to jump, that is the only question"
+  str1.erase(9, 4); // str1 =  "To be or to jump, that is the only question"
+  cout << str1 << endl;
+  for (int i = 0; i < str3.length(); i++)
+    cout << str3[i]; cout << endl;
+
+  // Result:
+  // To be or to jump, that is the only question
+  // or not to be
+}
