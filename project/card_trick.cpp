a4f644b01e387cb3edd05acd6e77d251abe9ae97
jsami <rj.sami21@outlook.com>
2017-07-09 10:38:47 +0400

    Exercice 7: card trick

---
+++ project/card_trick.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+const char digits[3] = {'A', 'B', 'C'};
+const char remainderNames[3] = {'X', 'Y', 'Z'};
+
+int Rotate(int input) {
+  int firstDigit = input / 100;
+  return (input % 100) * 10 + firstDigit;
+}
+
+int main() {
+  int input;
+  cout << "Input 3 digits number: "; cin >> input;
+  // manipulate remainders
+  int remainders[3];
+  for (int i = 0; i < 3; i++) {
+    remainders[i] = input % 11;
+    cout << "remainder of " << input << " : " << remainderNames[i] << " = " << remainders[i] << endl;
+    input = Rotate(input);
+  }
+
+  // manipulates sums
+  int sums[3];
+  int sumIndx = 0;
+  for (int i = 0; i < 2; i++) {
+    for (int j = i + 1; j < 3; j++) {
+      sums[sumIndx] = remainders[i] + remainders[j];
+      cout << remainderNames[i] << "+" << remainderNames[j] << " = " << sums[sumIndx] << endl;
+      if (sums[sumIndx] % 2) {
+        if (sums[sumIndx] < 9) {
+          sums[sumIndx] += 11;
+          cout << "sum is odd, increase by 11 => " << sums[sumIndx] << endl;
+        }
+        else if(sums[sumIndx] > 2) {
+          sums[sumIndx] -= 11;
+          cout << "sum is odd, decrease by 11 => " << sums[sumIndx] << endl;
+        }
+        else {
+          cout << "No need to increase/decrease by 11 nor halving." << endl;
+          continue;
+        }
+      }
+      sums[sumIndx] /= 2;
+      cout << "Half it: " << sums[sumIndx] << endl;
+      cout << "------------------------" << endl;
+      sumIndx++;
+    }
+  }
+  return 0;
+}
