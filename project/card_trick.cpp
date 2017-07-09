d35a4e91588d3aec4ea7393dff878eebe0b8cc28
jsami <rj.sami21@outlook.com>
2017-07-09 21:54:05 +0400

    perform decomposition int functions

--- project/card_trick.cpp
+++ project/card_trick.cpp
@@ -11,44 +11,66 @@ int Rotate(int input) {
   return (input % 100) * 10 + firstDigit;
 }
 
-int main() {
+int GetInput() {
   int input;
   cout << "Input 3 digits number: "; cin >> input;
-  // manipulate remainders
-  int remainders[kNumberOfDigits];
+  return input;
+}
+
+void PopulateRemainders(int* remainders, int input) {
   for (int i = 0; i < kNumberOfDigits; i++) {
     remainders[i] = input % 11;
     cout << "remainder of " << input << " : " << kRemainderNames[i] << " = " << remainders[i] << endl;
     input = Rotate(input);
   }
   cout << "------------------------" << endl;
+}
 
-  // manipulates sums
+void Half(int& n) {
+  n /= 2;
+  cout << "Half it: " << n << endl;
+  cout << "------------------------" << endl;
+}
+
+// return true if we hit 9. false otherwise
+bool AdjustOdd(int& n) {
+  if (!(n % 2))
+    return false;
+  if (n < 9) {
+    n += 11;
+    cout << "sum is odd, increase by 11 => " << n << endl;
+  }
+  else if (n > 9) {
+    n -= 11;
+    cout << "sum is odd, decrease by 11 => " << n << endl;
+  }
+  else { // We hit 9
+    cout << "No need to increase/decrease by 11 nor halving." << endl;
+    return true;
+  }
+  return false;
+}
+
+void SumRemaindersThenHalf(int* remainders) {
   int sums[kNumberOfDigits];
-  int sumIndx = 0;
-  for (int i = 0; i < kNumberOfDigits-1; i++) {
+  int sum_idx = 0;
+  for (int i = 0; i < kNumberOfDigits - 1; i++) {
     for (int j = i + 1; j < kNumberOfDigits; j++) {
-      sums[sumIndx] = remainders[i] + remainders[j];
-      cout << kRemainderNames[i] << "+" << kRemainderNames[j] << " = " << sums[sumIndx] << endl;
-      if (sums[sumIndx] % 2) {
-        if (sums[sumIndx] < 9) {
-          sums[sumIndx] += 11;
-          cout << "sum is odd, increase by 11 => " << sums[sumIndx] << endl;
-        }
-        else if (sums[sumIndx] > 9) {
-          sums[sumIndx] -= 11;
-          cout << "sum is odd, decrease by 11 => " << sums[sumIndx] << endl;
-        }
-        else {
-          cout << "No need to increase/decrease by 11 nor halving." << endl;
-          continue;
-        }
-      }
-      sums[sumIndx] /= 2;
-      cout << "Half it: " << sums[sumIndx] << endl;
-      cout << "------------------------" << endl;
-      sumIndx++;
+      sums[sum_idx] = remainders[i] + remainders[j];
+      cout << kRemainderNames[i] << "+" << kRemainderNames[j] << " = " << sums[sum_idx] << endl;
+      bool is_nine = AdjustOdd(sums[sum_idx]);
+      if(is_nine)
+        continue;
+      Half(sums[sum_idx]);
+      sum_idx++;
     }
   }
+}
+
+int main() {
+  int input = GetInput();
+  int remainders[kNumberOfDigits];
+  PopulateRemainders(remainders, input);
+  SumRemaindersThenHalf(remainders);
   return 0;
 }
